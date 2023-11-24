fbc9386fc39d2223575c840da536c123b762732c
bbtpls <144019848+bbtpls@users.noreply.github.com>
2023-11-24 18:31:39 +0800

    Done

--- Final Shiny app.R
+++ Final Shiny app.R
@@ -12,42 +12,39 @@ library(shiny)
 library(shinyjs)
 library(shinydashboard)
 library(readr)
+library(plotly)
 spyt <- read_csv("Spotify_Youtube.csv")
 
 #cleaning the relevant data
 cl_spyt <- spyt %>%
-  mutate(lg_Title = nchar(Title),
-         link_Desc = str_count(Description, "https://"),
-         tf_licens = as.integer(as.logical(spyt$Licensed)),
-         tf_offvid = as.integer(as.logical(spyt$official_video))) %>%
-  select(Instrumentalness, Liveness, Valence, Tempo, Duration_ms, Stream, lg_Title, Views, Likes, link_Desc, tf_licens, tf_offvid, Danceability, Energy, Speechiness)
+  mutate(Length_of_Title = nchar(Title),
+         Links_in_Description = str_count(Description, "https://"),
+         Licensed_Binary = as.integer(as.logical(spyt$Licensed)),
+         Official_Video_Binary = as.integer(as.logical(spyt$official_video))) %>%
+  select(Stream, Danceability, Energy, Speechiness, Instrumentalness, Liveness, Valence, Tempo, Duration_ms, Length_of_Title, Views, Likes, Comments, Links_in_Description, Licensed_Binary, Official_Video_Binary)
 
 
 ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
+  dashboardHeader(disable = TRUE),
+  dashboardSidebar(disable = TRUE),
   dashboardBody(
     useShinyjs(),
     fluidRow(
       box(selectInput("var_x", label = h2("select x-axis"), 
-                      choices = colnames(cl_spyt)),
-          selectInput("var_y", label = h2("select y-axis"), 
+                      choices = colnames(cl_spyt), selected = NULL),
+          selectInput("var_y", label = h2("select y-axis", selected = NULL), 
                       choices = colnames(cl_spyt)))
     ),
-    fluidRow(
-      box(plotOutput("line", height = 600, width = 900, click = "plot_click"))
-    ),
-    fluidRow(verbatimTextOutput("info"))
+    plotlyOutput("graph")
   )
 )
 
-server <- function(input, output) {
-  output$line<- renderPlot({
-    ggplot(cl_spyt) + aes_string(x=input$var_x, y=input$var_y) + geom_point()
+server <- function(input, output, session) {
+  output$graph <- renderPlotly({
+    plot_ly(cl_spyt, x=~get(input$var_x), y=~get(input$var_y), type = 'scatter', mode = 'markers')%>%
+      layout(title = paste('Plot of', input$var_y, 'against', input$var_x), xaxis = list(title = input$var_x), yaxis = list(title = input$var_y))
   })
-  output$info <- renderText({
-    paste0("x=", input$plot_click$x, "\ny=", input$plot_click$y)
-  })
+
   addClass(selector = "body", class = "sidebar-collapse")
 }
 
